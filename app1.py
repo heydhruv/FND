8c3c64000d05b096b69e798dca276ce9e70395c6
Dhruv Dave <71689864+heydhruv@users.noreply.github.com>
2022-04-07 10:22:47 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,55 @@
+from flask import Flask,render_template,request ,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+ 
+example_sent = """"""
+ 
+stop_words = set(stopwords.words('english'))
+ 
+word_tokens = word_tokenize(example_sent)
+ 
+filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+ 
+filtered_sentence = []
+ 
+for w in word_tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+ 
+print(word_tokens)
+print(filtered_sentence)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///detector.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class detector(db.Model):
+    sno=db.Column(db.Integer, primary_key=True)
+    description=db.Column(db.String(500),nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.description}"
+@app.route("/",methods=["GET", "POST"])
+def hello_world():       
+    if request.method =="POST":
+        print("post")
+        description=request.form['description']
+        detector= detector(description=description)
+
+       # db.session.add(detector)
+       # db.session.commit()
+        user =request.form["nm"]
+        return redirect(url_for("user",usr=user))
+    else:    
+        return render_template('index.html')
+ 
+#@app.route("/<usr>") 
+#def luser(usr):
+    #return f"<h1>{usr}</h1>"       
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
